27ec0ab13efece8489bb0de7aad89ff46ed496ec
Taro112233 <thanatouchth@gmail.com>
2025-06-27 11:51:15 +0700

    Remove all Prisma migrations and uploaded images
    Deleted all migration SQL files and the migration lock from the prisma/migrations directory, as well as all files in public/uploads. Updated schema.prisma to reflect the removal of migrations and to clean up model definitions. This resets the database migration history and removes all uploaded image assets.

--- prisma/migrations/20250626154950_mederrorimage_cascade_delete/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "MedErrorImage" DROP CONSTRAINT "MedErrorImage_medErrorId_fkey";
-
--- AddForeignKey
-ALTER TABLE "MedErrorImage" ADD CONSTRAINT "MedErrorImage_medErrorId_fkey" FOREIGN KEY ("medErrorId") REFERENCES "MedError"("id") ON DELETE CASCADE ON UPDATE CASCADE;
