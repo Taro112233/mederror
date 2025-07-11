9b901459f2f87e1444de6a9c4fb149fae9fe06d3
Taro112233 <thanatouchth@gmail.com>
2025-07-12 01:01:41 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { verifyJwtToken } from "@/lib/utils";
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
